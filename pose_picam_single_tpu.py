1bcdfb27f82ddd97a97bae134030898018a9e70b
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-08-18 14:16:01 +0900

    init

---
+++ pose_picam_single_tpu.py
@@ -0,0 +1,146 @@
+import argparse
+import numpy as np
+import cv2
+import time
+from PIL import Image
+from time import sleep
+from edgetpu.basic import edgetpu_utils
+from pose_engine import PoseEngine
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video.filevideostream import FileVideoStream
+import imutils
+
+lastresults = None
+processes = []
+frameBuffer = None
+results = None
+fps = ""
+detectfps = ""
+framecount = 0
+detectframecount = 0
+time1 = 0
+time2 = 0
+
+EDGES = (
+    ('nose', 'left eye'),
+    ('nose', 'right eye'),
+    ('nose', 'left ear'),
+    ('nose', 'right ear'),
+    ('left ear', 'left eye'),
+    ('right ear', 'right eye'),
+    ('left eye', 'right eye'),
+    ('left shoulder', 'right shoulder'),
+    ('left shoulder', 'left elbow'),
+    ('left shoulder', 'left hip'),
+    ('right shoulder', 'right elbow'),
+    ('right shoulder', 'right hip'),
+    ('left elbow', 'left wrist'),
+    ('right elbow', 'right wrist'),
+    ('left hip', 'right hip'),
+    ('left hip', 'left knee'),
+    ('right hip', 'right knee'),
+    ('left knee', 'left ankle'),
+    ('right knee', 'right ankle'),
+)
+
+
+def draw_pose(img, pose, threshold=0.2):
+    xys = {}
+    for label, keypoint in pose.keypoints.items():
+        if keypoint.score < threshold: continue
+        xys[label] = (int(keypoint.yx[1]), int(keypoint.yx[0]))
+        img = cv2.circle(img, (int(keypoint.yx[1]), int(keypoint.yx[0])), 5, (0, 255, 0), -1)
+
+    for a, b in EDGES:
+        if a not in xys or b not in xys: continue
+        ax, ay = xys[a]
+        bx, by = xys[b]
+        img = cv2.line(img, (ax, ay), (bx, by), (0, 255, 255), 2)
+
+
+def overlay_on_image(frames, result, model_width, model_height):
+
+    color_image = frames
+
+    if isinstance(result, type(None)):
+        return color_image
+    img_cp = color_image.copy()
+
+    for pose in result:
+        draw_pose(img_cp, pose)
+
+    cv2.putText(img_cp, fps,       (model_width-170,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (38,0,255), 1, cv2.LINE_AA)
+    cv2.putText(img_cp, detectfps, (model_width-170,30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (38,0,255), 1, cv2.LINE_AA)
+
+    return img_cp
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default="models/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite", help="Path of the detection model.")
+    parser.add_argument("--usbcamno", type=int, default=0, help="USB Camera number.")
+    parser.add_argument('--videofile', default="", help='Path to input video file. (Default="")')
+    parser.add_argument('--vidfps', type=int, default=30, help='FPS of Video. (Default=30)')
+    args = parser.parse_args()
+
+    model     = args.model
+    usbcamno  = args.usbcamno
+    vidfps    = args.vidfps
+    videofile = args.videofile
+
+    camera_width  = 320
+    camera_height = 240
+    model_width   = 640
+    model_height  = 480
+
+    engine = PoseEngine(model)
+    sleep(5)
+
+    if videofile == "":
+        vs = PiVideoStream((camera_width, camera_height), vidfps).start()
+        waittime = 1
+        window_name = "PiCamera"
+    else:
+        vs = FileVideoStream(video_file_path).start()
+        waittime = vidfps - 20
+        window_name = "Movie File"
+
+    cv2.namedWindow(window_name, cv2.WINDOW_AUTOSIZE)
+
+    while True:
+        t1 = time.perf_counter()
+
+        color_image = vs.read()
+
+        # Run inference.
+        color_image = cv2.resize(color_image, (model_width, model_height))
+        prepimg = color_image[:, :, ::-1].copy()
+
+        tinf = time.perf_counter()
+        res, inference_time = engine.DetectPosesInImage(prepimg)
+
+        if res:
+            detectframecount += 1
+            imdraw = overlay_on_image(color_image, res, model_width, model_height)
+        else:
+            imdraw = color_image
+
+        cv2.imshow(window_name, imdraw)
+
+        if cv2.waitKey(waittime)&0xFF == ord('q'):
+            break
+
+        # FPS calculation
+        framecount += 1
+        if framecount >= 15:
+            fps       = "(Playback) {:.1f} FPS".format(time1/15)
+            detectfps = "(Detection) {:.1f} FPS".format(detectframecount/time2)
+            framecount = 0
+            detectframecount = 0
+            time1 = 0
+            time2 = 0
+        t2 = time.perf_counter()
+        elapsedTime = t2-t1
+        time1 += 1/elapsedTime
+        time2 += elapsedTime
+

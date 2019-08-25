9b814c639639aab419110f620a191c612039792a
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-08-25 23:32:31 +0900

    Multi-Model+Multi-TPU added

--- models/download.sh
+++ models/download.sh
@@ -21,5 +21,4 @@ rm mobilenet_ssd_v2_coco.zip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1lAbSVsmG9ticeIicHqgDrBFru_pJeyzZ" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1lAbSVsmG9ticeIicHqgDrBFru_pJeyzZ" -o colorpalette.png
-unzip colorpalette.png
-rm colorpalette.png
+
